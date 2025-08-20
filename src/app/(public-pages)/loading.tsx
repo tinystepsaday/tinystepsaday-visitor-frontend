e67e6bcbd49e9d51830b964fc48f27d9ba6c130f
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-08-20 06:40:19 +0200

    feat: Add Loading component for enhanced user experience during data fetching; includes animated visuals and a logo for better loading feedback

---
+++ src/app/(public-pages)/loading.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Loading() {
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(120,108,255,0.15)] backdrop-blur-sm">
+            <div className="flex flex-col items-center gap-6">
+                <div className="relative flex items-center justify-center">
+                    <span className="absolute inline-block w-40 h-40 rounded-full bg-violet-400 opacity-30 animate-pulse-slow" />
+                    <span className="absolute inline-block w-56 h-56 rounded-full bg-violet-300 opacity-20 animate-pulse-slower" />
+                    <Image
+                        src="/tinystepsaday-logo.png"
+                        alt="TinyStepsADay Logo"
+                        width={100}
+                        height={100}
+                        className="z-10 animate-spin-slow drop-shadow-xl"
+                        priority
+                    />
+                </div>
+                {/* <span className="text-xl font-semibold text-violet-700 animate-fade-in">Loading Tiny Steps A Day...</span> */}
+            </div>
+        </div>
+    );
+}
